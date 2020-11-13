50fd5bdb3d5e9fd60cefd86134a4000e1cdea6e7
soltan <soltangh.work@gmail.com>
2020-11-13 18:54:31 +0200

    added login activity template

---
+++ app/src/main/java/dev/skyit/elearning/auth/AuthActivity.kt
@@ -0,0 +1,17 @@
+package dev.skyit.elearning.auth
+
+import android.os.Bundle
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import dev.skyit.elearning.R
+
+class AuthActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_auth)
+        setSupportActionBar(findViewById(R.id.toolbar))
+
+    }
+}
---
+++ app/src/main/java/dev/skyit/elearning/auth/SelectAccountFragment.kt
@@ -0,0 +1,22 @@
+package dev.skyit.elearning.auth
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.fragment.findNavController
+import dev.skyit.elearning.R
+
+/**
+ * A simple [Fragment] subclass as the second destination in the navigation.
+ */
+class SelectAccountFragment : Fragment(R.layout.fragment_select_account) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+
+}
