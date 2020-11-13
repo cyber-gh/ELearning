fcdd637348da2ff3754ce9b023c21857fc5eb1bf
soltan <soltangh.work@gmail.com>
2020-11-13 17:27:18 +0200

    added gitignore

--- app/src/main/java/dev/skyit/elearning/MainActivity.kt
+++ app/src/main/java/dev/skyit/elearning/MainActivity.kt
@@ -24,6 +24,6 @@ class MainActivity : AppCompatActivity() {
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
 
-        
+
     }
 }
